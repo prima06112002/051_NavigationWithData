c5bb57bd9ddba8c65d28326b4ba190715d186a40
Prima06112002 <ramadhan06112002@gmail.com>
2023-11-15 19:59:53 +0700

    menambahkan text val di orderuistate dataclass

--- app/src/main/java/com/example/estehpam/data/OrderUIState.kt
+++ app/src/main/java/com/example/estehpam/data/OrderUIState.kt
@@ -3,5 +3,9 @@ package com.example.estehpam.data
 data class OrderUIState(
     val jumlah: Int = 0,
     val rasa: String = "",
-    val harga: String = ""
+    val harga: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+
 )
